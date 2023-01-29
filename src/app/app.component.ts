bc2cfff1c4f2612aa2f6e7e3649cab4626e3541e
Maciej Gwizdz <maciej.gwizdz1994@gmail.com>
2023-01-29 19:30:39 +0100

    initial project with components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'complete-guide-basics-project';
+export class AppComponent {  
 }
